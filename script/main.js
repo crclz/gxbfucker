db44311218a5e2306bfb7d4a488dd2654f6e816b
crucialize <crucialize@126.com>
2019-11-05 00:12:43 +0800

    init

---
+++ script/main.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         gxbfucker
+// @namespace    https://github.com/crclz
+// @version      0.1
+// @description  高校邦浏览器端脚本
+// @author       crclz
+// @match        https://*.gaoxiaobang.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    document.title = "pick your shit";
+    setTimeout(() => {
+        close()
+    }, 1000)
+})();
