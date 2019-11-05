313c54a690133b2445e0202908d042842ab32bb1
crucialize <crucialize@126.com>
2019-11-05 12:31:27 +0800

    complete

--- script/main.js
+++ script/main.js
@@ -43,6 +43,8 @@ class JsExt {
     }
 }
 
+const catVideo17minUrl = 'http://localhost:3000';
+
 // Be attention: main is an async function.
 (async function () {
     'use strict';
@@ -50,13 +52,55 @@ class JsExt {
     // Wait until video is loaded or keep waiting if there's no video.
     await JsExt.waitUntil(() => PageUtils.getOneVideo())
 
+    // Wait gxb's js code to load normal video
+    await JsExt.wait(500)
+
     let video0 = PageUtils.getOneVideo()
 
-    if (video0) {
-        PageUtils.setTitle('has-video')
-    } else {
-        throw new Error('Should have video')
-    }
+    // Prevent the page from redirecting when video ends
+    // TODO: test if can use video0 instead of window (window works)
+    PageUtils.blockEndedEvent(window)
 
+    // In order to play video from local server
+    video0.removeAttribute('crossorigin')
+
+    // Set the video source
+    video0.src = catVideo17minUrl
+
+    // Override pause function
+    video0.pause = () => { }
+
+    video0.play()
+
+    // Do some periodical tasks
+    setInterval(() => {
+        // Close the shit dialog
+        $(".gxb-dialog-close").click()
+
+        // Check if 100%
+        var learnPercent = $('span.video-percent').text()
+        if (learnPercent === '100') {
+            // Close page after 5 seconds
+            setTimeout(() => close(), 5000)
+        }
+
+    }, 200)
+
+    // Register mid-click on chapter list
+    $(".chapter-info").on('mousedown', function (e) {
+        if ((e.which == 2)) {
+            // Middle click
+            let chapterId = $(this).attr('chapter_id')
+
+            let url = location.href
+            url = url.match('(.+/unit/.+/chapter/)[0-9]+')[1]
+
+            url += String(chapterId)
+
+            console.log(url)
+
+            window.open(url)
+        }
+    })
 
 })();
